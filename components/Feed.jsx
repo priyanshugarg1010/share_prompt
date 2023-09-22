be1c0dd2ab7c8fb4e9bab51a3e73469f9b1ccb6b
Priyanshu Garg <priyanshugarg1810@gmail.com>
2023-09-22 13:03:08 +0530

    added the functionality to search bar

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import PromptCard from "@components/PromptCard";
 
 export const PromptCardList = ({ data, handleTagClick }) => {
@@ -19,10 +19,33 @@ export const PromptCardList = ({ data, handleTagClick }) => {
 
 const Feed = () => {
   const [searchedText, setSearchedText] = useState("");
+  const [searchTimeOut, setSearchTimeOut] = useState(null);
+  const [searchResult, setSearchResult] = useState([]);
   const [posts, setPosts] = useState([]);
+  const [searchEx, setSearchEx] = useState(false);
+  const searchInputRef = useRef(null);
 
-  const handleSearchedText = (e) => {};
-  const handleTagClick = () => {};
+  const handleSearchedText = (e) => {
+    clearTimeout(searchTimeOut);
+
+    setSearchedText(e.target.value);
+    setSearchEx(false);
+    setSearchTimeOut(
+      setTimeout(() => {
+        const result = filterPrompts(e.target.value);
+        setSearchResult(result);
+      }, 500)
+    );
+  };
+  const handleTagClick = (tagName) => {
+    setSearchedText(tagName);
+
+    const result = filterPrompts(tagName);
+    setSearchEx(ture);
+
+    setSearchResult(result);
+    searchInputRef.current.blur();
+  };
 
   const fetchPosts = async () => {
     const res = await fetch("/api/prompt");
@@ -34,20 +57,52 @@ const Feed = () => {
     fetchPosts();
   }, []);
 
+  const filterPrompts = (searchedText) => {
+    const regex = new RegExp(searchedText, "i");
+
+    return posts.filter(
+      (item) =>
+        regex.test(item.creator.username) ||
+        regex.test(item.tag) ||
+        regex.test(item.prompt)
+    );
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Prevent the default form submission behavior
+    // You can choose to perform the search here if needed (e.g., setSearchResult())
+    // setSearchResult();
+
+    setSearchEx(true);
+    searchInputRef.current.blur();
+  };
+
   return (
     <>
       <section className="feed">
-        <form className="relative w-full flex-center">
+        <form className="relative w-full flex-center " onSubmit={handleSubmit}>
           <input
             type="text"
             placeholder="search for a tag or a username"
             value={searchedText}
             onChange={handleSearchedText}
             required
-            className="search_input peer"
+            className={`search_input peer ${searchEx ? "search_executed" : ""}`}
+            ref={searchInputRef}
           />
         </form>
-        <PromptCardList data={posts} handleTagClick={handleTagClick} />
+        {searchedText ? (
+          searchResult && searchResult.length > 0 ? ( // Check if searchResult is not empty
+            <PromptCardList
+              data={searchResult}
+              handleTagClick={handleTagClick}
+            />
+          ) : (
+            <p>No results found.</p> // Display a message if searchResult is empty
+          )
+        ) : (
+          <PromptCardList data={posts} handleTagClick={handleTagClick} />
+        )}
       </section>
     </>
   );
